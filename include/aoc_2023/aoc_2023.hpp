b5eb7b717d2232619b4ba801d7e394f0fbfac5bd
Tero <contact.tero@gmail.com>
2023-12-13 01:11:22 +0100

    add 3 separate executable - one for executing the current year problems - one for executing all years problems - one for working on the solution (workbench)

---
+++ include/aoc_2023/aoc_2023.hpp
@@ -0,0 +1,8 @@
+#include <chrono>
+#include <string>
+
+#include <aoc/aoc.hpp>
+
+namespace aoc_2023 {
+template<uint8_t _day> using day = aoc::day_for_year<2023, _day>;
+}// namespace aoc_2023
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <aoc/utils.hpp>
+
+#include "aoc_2023/all_days.hpp"
+
+int main() { aoc::utils::solve_and_print(aoc_2023::day_01{}); }
